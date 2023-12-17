731938687ec3afc13b7d9e886f45cf3fb38b1f68
Dave Rintoul <drintoul@gmail.com>
2023-12-17 17:13:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,28 +8,29 @@ import sweetviz as sv
 
 sv.config_parser.read("override.ini")
 
-def st_display_sweetviz(report_html,width=1000,height=500):
-	report_file = codecs.open(report_html,'r')
+def st_display_sweetviz(report_html, width=1000, height=500):
+
+	report_file = codecs.open(report_html, 'r')
 	page = report_file.read()
-	components.html(page,width=width,height=height,scrolling=True)
+	components.html(page, width=width, height=height, scrolling=True)
 
 
 def main():
 
-    st.set_page_config(page_title="Exploratory Data Analysis")
-    st.title("Exploratory Data Analysis")
+	st.set_page_config(page_title="Exploratory Data Analysis")
+	st.title("Exploratory Data Analysis")
 
-    # CSV file upload
-    csv_file = st.file_uploader("Upload a CSV file", type=["csv"])
+	# CSV file upload
+	csv_file = st.file_uploader("Upload a CSV file", type=["csv"])
 
-    if csv_file:
-	    
-      df = pd.read_csv(csv_file)
-      #st.dataframe(df.head(), hide_index=True)
+	if csv_file:
 
-      report = sv.analyze(df)
-      report.show_html()
-      st_display_sweetviz("SWEETVIZ_REPORT.html")
+		df = pd.read_csv(csv_file)
+		#st.dataframe(df.head(), hide_index=True)
+
+		report = sv.analyze(df)
+		report.show_html()
+		st_display_sweetviz("SWEETVIZ_REPORT.html")
 
 if __name__ == '__main__':
-    main()		
+	main()		
