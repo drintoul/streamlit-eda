1f3623ac66d14608c552542301041210d786b376
Dave Rintoul <drintoul@gmail.com>
2023-12-17 16:56:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
-#import streamlit.components.v1 as components
 #st.set_page_config(layout="wide")
+
+import streamlit.components.v1 as components
 import pandas as pd
 import codecs
 import sweetviz as sv
