6653a655c826dfb21f9e054936c228b47385420c
Dave Rintoul <drintoul@gmail.com>
2023-12-17 16:13:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,24 @@
 import streamlit as st
+#st.set_page_config(layout="wide")
+import pandas as pd
 
-st.write("hello")
+def on_text_area_change():
+
+    st.session_state.page_text = st.session_state.my_text_area
+    
+
+def main():
+
+    st.set_page_config(page_title="Exploratory Data Analysis")
+    st.title("Exploratory Data Analysis")
+
+    # CSV file upload
+    csv_file = st.file_uploader("Upload a CSV file", type=["csv"])
+
+    if csv_file:
+
+      df = pd.read_csv(csv_file)
+      st.dataframe(csv_file.head())
+
+if __name__ == '__main__':
+    main()
